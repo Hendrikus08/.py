7d5bf614bd931d2ea147026b524fdd07c575b799
Hendrikus08 <104079352+Hendrikus08@users.noreply.github.com>
2022-04-21 01:24:25 +0700

    Create .py

---
+++ .py
@@ -0,0 +1,10 @@
+nama : Hendrikus sudarso
+nim 191011400802
+nim genap
+Nim genap: tutorial kondisi (if else
+                            totorial if else
+                             
+                             
+                             
+                             
+                             https://youtu.be/39aQiBI_83c
